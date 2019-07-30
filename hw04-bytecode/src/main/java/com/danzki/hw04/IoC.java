abdeb44c107c91ab8700d17d8a885a1ea5eb0a7d
Dan.Kapustin <dan.kapustin@gmail.com>
2019-07-30 23:59:37 +0300

    Change after review

--- hw04-bytecode/src/main/java/com/danzki/hw04/IoC.java
+++ hw04-bytecode/src/main/java/com/danzki/hw04/IoC.java
@@ -3,7 +3,6 @@ package com.danzki.hw04;
 import com.danzki.hw04.Annotations.Log;
 import com.danzki.hw04.Interfaces.ApplicationInterface;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -25,8 +24,9 @@ public class IoC {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-      Method customMethod = getMatchingMethodOnGivenAnnotation(method, (ApplicationImpl) appClass, Log.class);
-      if (customMethod != null) {
+      if (appClass.getClass()
+            .getDeclaredMethod(method.getName(), method.getParameterTypes())
+            .isAnnotationPresent(Log.class)) {
         System.out.print("executed method:" + method.getName() + ", ");
         int c = 0;
         for (Object arg : args) {
@@ -35,18 +35,6 @@ public class IoC {
         System.out.println("");
       }
       return method.invoke(appClass, args);
-    }
-
-    private Method getMatchingMethodOnGivenAnnotation(Method method, ApplicationInterface classProxied, Class<? extends Annotation> annotation) {
-      try {
-        Method customMethod = classProxied.getClass().getDeclaredMethod(method.getName(), method.getParameterTypes());
-        if (customMethod.isAnnotationPresent(annotation)) {
-          return customMethod;
-        }
-        return null;
-      } catch (NoSuchMethodException e) {
-        return null;
-      }
     }
   }
 
